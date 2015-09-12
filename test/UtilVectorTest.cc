415e7316abae3121b0b3c310a4b4f2c6b9e7f425
Oscar Raig <oscar.raig@gmail.com>
2015-09-13 00:20:03 +0200

    creating first test

--- test/UtilVectorTest.cc
+++ test/UtilVectorTest.cc
@@ -1,6 +1,14 @@
 #include "gtest/gtest.h"
+#include "UtilVector.hpp"
 
 
-TEST(FactorialTest, Zero) {
-  EXPECT_EQ(1, 2);
+TEST(MoveArrayOnePositionLeft, Zero) {
+ 	double *x = NULL,*y = NULL;
+	int nCounter = 1 ;
+	int nSize = 4;
+	x = (double*)malloc(nSize * sizeof(double));
+	y = (double*)malloc(nSize * sizeof(double));
+	
+  	UtilVector::MoveArrayOnePositionLeft( x, y, nCounter, &nSize);	
+  	EXPECT_EQ(nSize, 3);
 }
