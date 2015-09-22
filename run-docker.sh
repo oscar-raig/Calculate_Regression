c4169178914c2270452e99bdd534fe86f8eb6db0
Oscar Raig <oscar.raig@gmail.com>
2015-09-22 07:51:32 +0200

    Adding travis

--- run-docker.sh
+++ run-docker.sh
@@ -1,1 +1,1 @@
-docker run   -ti --entrypoint=/bin/bash  -v /Users/oscarraig/git-public/Calculate_Regression/:/home/  oscarraig/c7-boost-gtest
+docker run   -ti --entrypoint=/bin/bash  -v `pwd`:/home/  oscarraig/c7-boost-gtest
